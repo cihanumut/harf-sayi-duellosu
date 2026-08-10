72d61b4a5a1daa46d67958e20153e7c0b3cdd94c
cihanumut <cihanumut_colak@hotmail.com>
2026-08-10 18:49:53 +0300

    feat: add service worker configuration for offline caching and asset management

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.7a466nu7e8g"
+    "revision": "0.6d07cehq5do"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
